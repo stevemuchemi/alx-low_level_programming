6992470bed1f974a67b0d60c7e61690bd728bd60
stevemuchemi <stephenmuchemi058@gmail.com>
2022-06-03 18:18:31 -0700

    bettttty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
 /**
- comment */
+ *main - Entry point
+ *
+ *Return:Always 0 (Success)
+*/
 
-int main (void)
+int main(void)
 {
-  puts ("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
